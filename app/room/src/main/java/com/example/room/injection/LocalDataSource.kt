63001c5d9582a6051c6a6395db13a45eb67db8d2
atkmehmet <durmazmehmet47@gmail.com>
2025-02-26 00:35:07 +0300

    add room

--- app/room/src/main/java/com/example/room/injection/LocalDataSource.kt
+++ app/room/src/main/java/com/example/room/injection/LocalDataSource.kt
@@ -1,5 +1,8 @@
 package com.example.room.injection
 
+import com.example.room.source.ImageDataSourceImpl
+import com.example.secretimage.domain.repository.ImageDataSource
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,5 +10,8 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LocalDataSource {
-
+    @Binds
+    abstract fun bindUserDataSource
+                (imageDataSourceImpl: ImageDataSourceImpl):
+            ImageDataSource
 }
