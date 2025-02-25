6ea08ece2532e9fa0fdb006211535094ce2fa339
atkmehmet <durmazmehmet47@gmail.com>
2025-02-26 00:27:29 +0300

    add room

--- app/room/src/main/java/com/example/room/source/ImageDataSourceImpl.kt
+++ app/room/src/main/java/com/example/room/source/ImageDataSourceImpl.kt
@@ -1,9 +1,11 @@
 package com.example.room.source
 
 import com.example.room.db.image.ImageDao
+import com.example.room.db.image.ImageEntity
 import com.example.secretimage.domain.repository.ImageDataSource
 import com.example.secretimage.model.Image
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ImageDataSourceImpl  @Inject constructor(
@@ -12,10 +14,25 @@ val imageDao: ImageDao
 {
 
     override fun getImage(): Flow<List<Image>> {
-        TODO("Not yet implemented")
+        return imageDao.getImages() // This returns Flow<List<ImageEntity>>
+            .map { imageEntityList ->
+                imageEntityList.map { imageEntity ->
+                    imageEntity.toImage() // Convert ImageEntity to Image
+                }
+            }
     }
 
-    override suspend fun addImage(image: Image) {
-        TODO("Not yet implemented")
+    fun ImageEntity.toImage(): Image {
+        return Image(
+            id = this.id,
+            name = this.name,
+            timestamp = this.timestamp,
+            image = this.image)
     }
+    override suspend fun addImage(image: Image) =
+        imageDao.insertImage(ImageEntity(
+           name =  image.name,
+           timestamp =   image.timestamp,
+            image = image.image
+        ))
 }
